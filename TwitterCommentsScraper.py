00daea1e9f4aa88409534b9b76f1b46e12f3905b
pnadelofficial <75741790+pnadelofficial@users.noreply.github.com>
2023-05-15 16:07:46 -0400

    Update TwitterCommentsScraper.py

--- TwitterCommentsScraper.py
+++ TwitterCommentsScraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
