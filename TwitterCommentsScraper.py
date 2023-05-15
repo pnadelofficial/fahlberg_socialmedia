751200bdd60c0d0ed81b7a8b0df435f5b32120b3
pnadelofficial <75741790+pnadelofficial@users.noreply.github.com>
2023-05-15 16:22:35 -0400

    Update TwitterCommentsScraper.py

--- TwitterCommentsScraper.py
+++ TwitterCommentsScraper.py
@@ -15,8 +15,13 @@ st.write("Input the URL to a Tweet below and wait a couple minutes. Once complet
 chrome_options = Options()
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 chrome_options.add_argument(f'user-agent={user_agent}')
-chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-features=NetworkService")
+chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("--disable-features=VizDisplayCompositor")
 
 @st.cache_resource
 def get_driver():
